f6c9fdda2437a72c89e0fd7d984b477fdae365d1
Terrywu0208 <75594915+Terrywu0208@users.noreply.github.com>
2022-05-18 20:19:45 +0800

    update

--- class.py
+++ class.py
@@ -57,7 +57,7 @@ def cut_img():
     cv2.waitKey(0)
 
 def usual():
-    img = cv2.imread(r"img\ford_mustang.png")
+    img = cv2.imread(r"img\paper0001-06.jpg")
     img = cv2.resize(img , (0,0), fx=1.5, fy= 1.5)
     kernel = np.ones((3,3),np.uint8)
 
@@ -105,28 +105,6 @@ cv2.createTrackbar("Sat Max" , "TrackBar", 255, 255, empty)
 cv2.createTrackbar("Val Min" , "TrackBar", 0, 255, empty)
 cv2.createTrackbar("Val Max" , "TrackBar", 255, 255, empty)
 
-while True:
-    ref, img = cap.read()
-    hsv = cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
-    h_min = cv2.getTrackbarPos("Hue Min" , "TrackBar")
-    h_max = cv2.getTrackbarPos("Hue Max" , "TrackBar")
-    s_min = cv2.getTrackbarPos("Sat Min" , "TrackBar")
-    s_max = cv2.getTrackbarPos("Sat Max" , "TrackBar")
-    v_min = cv2.getTrackbarPos("Val Min" , "TrackBar")
-    v_max = cv2.getTrackbarPos("Val Max" , "TrackBar")
-    print(h_min, h_max, s_min, s_max, v_min, v_max)
-
-    lower = np.array([h_min, s_min, v_min])
-    upper = np.array([h_max, s_max, v_max])
-
-    mask = cv2.inRange(hsv , lower, upper)
-
-    result = cv2.bitwise_and(img, img, mask = mask)
-
-    cv2.imshow("img",img)
-    cv2.imshow("result",result)
-    cv2.waitKey(10)
-        
 def detcet_geometry():
     img = cv2.imread(r"img\geometry.png")
     img = cv2.resize(img,(0,0), fx=2, fy=2)
@@ -158,6 +136,32 @@ def detcet_geometry():
     cv2.imshow("imgContour", imgContour)
     cv2.waitKey(0)
 
+# while True:
+#     # ref, img = cap.read()
+#     img = cv2.imread("img/paper0001-06.jpg")
+#     img = cv2.resize(img,None,fx=0.2,fy=0.2,interpolation=cv2.INTER_LINEAR)
+#     hsv = cv2.cvtColor(img , cv2.COLOR_BGR2HSV)
+#     h_min = cv2.getTrackbarPos("Hue Min" , "TrackBar")
+#     h_max = cv2.getTrackbarPos("Hue Max" , "TrackBar")
+#     s_min = cv2.getTrackbarPos("Sat Min" , "TrackBar")
+#     s_max = cv2.getTrackbarPos("Sat Max" , "TrackBar")
+#     v_min = cv2.getTrackbarPos("Val Min" , "TrackBar")
+#     v_max = cv2.getTrackbarPos("Val Max" , "TrackBar")
+#     print(h_min, h_max, s_min, s_max, v_min, v_max)
+
+#     lower = np.array([h_min, s_min, v_min])
+#     upper = np.array([h_max, s_max, v_max])
+
+#     mask = cv2.inRange(hsv , lower, upper)
+
+#     result = cv2.bitwise_and(img, img, mask = mask)
+
+#     cv2.imshow("img",img)
+#     cv2.imshow("result",result)
+#     cv2.waitKey(10)
+        
+
+usual()
 
 
 
